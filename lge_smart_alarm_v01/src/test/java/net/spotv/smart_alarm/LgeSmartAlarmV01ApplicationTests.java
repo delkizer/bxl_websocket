3a80c5fa38eee8c9fdd12634cf1ea5eba0446148
islan78 <island78@spotv.net>
2023-10-02 12:17:22 +0900

    초기 등록

---
+++ lge_smart_alarm_v01/src/test/java/net/spotv/smart_alarm/LgeSmartAlarmV01ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.spotv.smart_alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LgeSmartAlarmV01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
