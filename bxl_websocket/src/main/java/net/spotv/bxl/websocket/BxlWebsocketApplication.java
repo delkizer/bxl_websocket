2bb94d98e6e82d7546bff144e1d999db80530267
islan78 <island78@spotv.net>
2025-03-20 17:18:13 +0900

    초기 등록

---
+++ bxl_websocket/src/main/java/net/spotv/bxl/websocket/BxlWebsocketApplication.java
@@ -0,0 +1,13 @@
+package net.spotv.bxl.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BxlWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BxlWebsocketApplication.class, args);
+	}
+
+}
---
+++ bxl_websocket/src/test/java/net/spotv/bxl/websocket/BxlWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package net.spotv.bxl.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BxlWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
